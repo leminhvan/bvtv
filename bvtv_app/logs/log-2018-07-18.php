216186c9bc3192336f7ab16a4cb8a891dff2c191
leminhvan <leminhvan.92@gmail.com>
2018-07-18 11:32:36 +0700

    change

---
+++ bvtv_app/logs/log-2018-07-18.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-07-18 04:32:06 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) E:\host\www\bvtv\system_bvtv\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-07-18 04:32:06 --> Unable to connect to the database
